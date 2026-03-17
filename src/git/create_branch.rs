02f17e307cbb360d0424bf1c7b45064e867a48be
Kevin Goslar <kevin.goslar@gmail.com>
2026-03-16 20:55:30 -0500

    co-bot.toml create_branch.rs

--- src/git/create_branch.rs
+++ src/git/create_branch.rs
@@ -2,7 +2,7 @@ use crate::errors::{Result, UserError};
 use crate::subshell;
 
 pub fn create_branch(template: &str, name: &str) -> Result<()> {
-    let command = template.replace("{{workspace}}", name);
+    let command = template.replace("{{branch}}", name);
     subshell::run(&command).map_err(|err| UserError::CannotCreateBranch {
         command: command.to_string(),
         err: err.to_string(),
